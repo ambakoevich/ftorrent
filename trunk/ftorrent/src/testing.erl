a09a34c33c0b8f97db75a658fbf8f8fb8ee421d5
ambakoevich@gmail.com <ambakoevich@gmail.com@c2e23ef6-c506-1adb-5517-12d39c674ce0>
2011-10-03 15:53:46 +0000

    1st Commit

---
+++ trunk/ftorrent/src/testing.erl
@@ -0,0 +1,27 @@
+%% Author: David
+%% Created: 3 Oct 2011
+%% Description: TODO: Add description to testing
+-module(testing).
+
+%%
+%% Include files
+%%
+
+%%
+%% Exported Functions
+%%
+-export([plus/1]).
+
+
+%%
+%% API Functions
+%%
+
+
+
+%%
+%% Local Functions
+%%
+
+plus(N) ->
+	N+1.
