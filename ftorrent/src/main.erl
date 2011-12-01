b3aaa4150d217ccb18f7c88f81e6323d0e41e911
ionut.trancioveanu@gmail.com <ionut.trancioveanu@gmail.com@c2e23ef6-c506-1adb-5517-12d39c674ce0>
2011-12-01 12:11:41 +0000

    main with f-torrent logo

--- ftorrent/src/main.erl
+++ ftorrent/src/main.erl
@@ -6,6 +6,15 @@
 -include("constants.hrl").
 
 start() ->
+
+    io:format(" ______                                                       \n"),
+    io:format("|  ____|         _                                      _    \n"),          
+    io:format("| |___         _| |_   __   _ __  _ __   __    _ __   _| |_  \n"),
+    io:format("|  ___|  ____ |_   _| / _ \ | '__|| '__|/'__'| | '_  ||_   _| \n"),
+    io:format("| |     |____|  | |_ ( (_))| |   | |   |  __/ | | | |  | |_  \n"),
+    io:format("|_|             |___| \ __/ |_|   |_|   |____| |_| |_|  |___| \n\n\n"),
+          
+
     Manager = spawn(manager, start, []),
     GUI = spawn(gui, start, [Manager]),
     register(gui,GUI).   
