a266c88b3631177370759d3d03a042234cc39307
ambakoevich@gmail.com <ambakoevich@gmail.com@c2e23ef6-c506-1adb-5517-12d39c674ce0>
2011-12-02 15:25:02 +0000

    tracker.erl commented using edoc.

--- ftorrent/src/tracker.erl
+++ ftorrent/src/tracker.erl
@@ -1,17 +1,18 @@
-%% Author: Rashid Darwish
-%% Created: 2011-10-20
-%% Description: Establishing the connection with torrents trackers.
+%% @author Rashid Darwish
+%% @copyright Framework Studio
+%% @version v0.1
+%% @doc Created: 20-Oct-2011, Tracker establishes connection with torrent trackers.
 
 -module(tracker).
 -export([start/1]).
 
- %% Running the module
+%% @doc Starting the module
 start(URL)->
     io:format("URL RES: ~p~n",[URL]),
     get_request(URL).
 
- %% This function will call inets OTP and run the start function, 
- %% inorder to use httpc:request which is http get request
+%% @doc get_request will call inets OTP and run the start function, 
+%% inorder to use httpc:request which is http get request
 get_request(Url)-> 
     inets:start(),
     {ok, Result} = httpc:request(Url),
