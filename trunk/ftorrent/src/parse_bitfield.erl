1f5f91e7e7e29a4acd722de90fa29eec933b24db
ambakoevich@gmail.com <ambakoevich@gmail.com@c2e23ef6-c506-1adb-5517-12d39c674ce0>
2011-12-02 13:03:58 +0000

    parse_bitfield.erl commented according to edoc.

--- trunk/ftorrent/src/parse_bitfield.erl
+++ trunk/ftorrent/src/parse_bitfield.erl
@@ -1,13 +1,13 @@
-%% PARSING A BITFIELD
+%% @author Batbilig Bavuudorj, David Giorgidze
+%% @copyright Framework Studio
+%% @version v0.1
+%% @doc Created: 28-Nov-2011, Parse bitfield extracts indexes of pieces
+%% another peer 'has'.
+
 -module(parse_bitfield).
 -export([decimal_to_bit/2, concat_list/1, define_index/2]).
 
-
-%% The following codes are used to extract number of 'have' pieces from the bitfield received from a peer reply. 
-%%
-
-%% Convert a decimal into a list of bits.
-
+%% @doc Convert a decimal into a list of bits.
 decimal_to_bit(0, Acc) ->
     case length(Acc) of
 	8 ->
@@ -15,6 +15,7 @@ decimal_to_bit(0, Acc) ->
 	_Other ->
 	    decimal_to_bit(0, [0 | Acc])
 		end;
+
 decimal_to_bit(Bin, Acc) ->    
     case Bin rem 2 of
 	1 ->
@@ -23,21 +24,17 @@ decimal_to_bit(Bin, Acc) ->
 	    decimal_to_bit(Bin div 2, [0 | Acc])
 		end.
 
-
-%% Concatenate lists of bits/numbers. Arguments are a list of binary, and [] as an accumulator.
-
+%% @doc Concatenate lists of bits/numbers.
+%% Arguments are a list of binary, and [] as an accumulator.
 concat_list(Bin) ->
     L = binary_to_list(Bin),
     concat_list(L, []).
-  
 concat_list([], Acc) ->
     define_index(Acc, 0);
 concat_list([H|T], Acc) ->
     concat_list(T, Acc++decimal_to_bit(H,[])).
 
-
-%% Find a list of indices/pieces a peer has.
-
+%% @doc Find a list of indices/pieces a peer has.
 define_index([], _Index) ->
     [];
 define_index([H|T], Index) -> 
@@ -47,8 +44,6 @@ define_index([H|T], Index) ->
 	0 ->
 	    define_index(T, Index+1)
 		end.
-
-
 
 %% Run from the shell:
 %%
