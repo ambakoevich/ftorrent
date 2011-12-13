f60d51c0c13ef54b2e00ce908152f1751d3dc87f
ambakoevich@gmail.com <ambakoevich@gmail.com@c2e23ef6-c506-1adb-5517-12d39c674ce0>
2011-12-13 13:57:52 +0000

    io_manager.erl commented according to edoc.

--- ftorrent/src/io_manager.erl
+++ ftorrent/src/io_manager.erl
@@ -1,4 +1,9 @@
-%%IO MANAGER
+%% @author Rashid Darwish.
+%% @copyright Framework Studio.
+%% @version v0.1
+%% @doc Created: 18-Nov-2011, Io_manager is responsible for writing data to file. 
+%% Binary data is received from piece_manager and is written to file.
+
 -module(io_manager).
 -compile(export_all).
 
@@ -12,7 +17,6 @@ loop()->
             pm ! {select_piece, Pid},
 	    loop()
 end.
-
 
 print_to_file(Name,Offset,Data)-> 
     {ok, File} = file:open(Name,[read,write,raw,binary]),
